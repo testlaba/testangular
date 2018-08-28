b035ed9885c054013a7e5a51702c69ec3c40f4c5
AlexVH <alex.box@list.ru>
2018-08-28 17:54:28 +0300

    seach & view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {JsonFilmsDbService} from './services/json-films-db.service';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { FilmSearchComponent } from './components/film-search/film-search.component';
+import { FilmsListComponent } from './components/films-list/films-list.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { FilmDetailsDialogComponent } from './components/film-details-dialog/film-details-dialog.component';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent },
+  { path: 'filmsearch', component: FilmSearchComponent },
+  { path: 'filmlist', component: FilmsListComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmSearchComponent,
+    FilmsListComponent,
+    AppNavComponent,
+    HomePageComponent,
+    FilmDetailsDialogComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule
   ],
-  providers: [],
+  entryComponents: [FilmDetailsDialogComponent],
+  providers: [JsonFilmsDbService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // web API url (json-server)
+  apiUrl: 'http://localhost:3000'
 };
